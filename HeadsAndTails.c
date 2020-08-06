a0d4d67986e154100ad53df0a13d7e4eebc4c7b8
Zaki-K <zakiyama5785@gmail.com>
2020-08-06 21:20:07 +0900

    HoT: Initial revision

---
+++ HeadsAndTails.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+	int coin, i, h, t;
+	h = t = 0;
+	printf("Tossing a coin...\n");
+	for (i = 0; i < 3; i++) {
+		srand((unsigned int)time(NULL));
+		coin = rand() % 2;
+		printf("Round %d: ", i + 1);
+		if (coin == 0) {
+			printf("Heads\n");
+			h++;
+		} else {
+			printf("Tails\n");
+			t++;
+		}
+		sleep(1);
+	}
+
+	printf("Heads: %d, Tails: %d\n", h, t);
+
+	return 0;
+}

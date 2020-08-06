870e36b2ec328a8df184e52d3f8dd88c1f3714b6
Zaki-K <zakiyama5785@gmail.com>
2020-08-06 21:41:30 +0900

    Final revision

--- HeadsAndTails.c
+++ HeadsAndTails.c
@@ -28,9 +28,9 @@ int main() {
 
 	printf("Heads: %d, Tails: %d\n", h, t);
 	if (h > t)
-		printf("You won!\n");
+		printf("%s won!\n", name);
 	else
-		printf("You lost..\n");
+		printf("%s lost..\n", name);
 
 	return 0;
 }
